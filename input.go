96d4f68d5b3615dccb3c6e28d2e9932043a32b12
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-05-01 13:24:16 +0700

    Working joystick #1

--- input.go
+++ input.go
@@ -20,7 +20,25 @@ var keyBinds = map[glfw.Key]uint32{
 	glfw.KeyBackspace: libretro.DeviceIDJoypadSelect,
 }
 
-var joy [libretro.DeviceIDJoypadR3 + 1]bool
+var buttonBinds = map[byte]uint32{
+	0: libretro.DeviceIDJoypadUp,
+	1: libretro.DeviceIDJoypadDown,
+	2: libretro.DeviceIDJoypadLeft,
+	3: libretro.DeviceIDJoypadRight,
+	4: libretro.DeviceIDJoypadStart,
+	5: libretro.DeviceIDJoypadSelect,
+	6: libretro.DeviceIDJoypadL3,
+	7: libretro.DeviceIDJoypadR3,
+	8: libretro.DeviceIDJoypadL,
+	9: libretro.DeviceIDJoypadR,
+	//10: libretro.DeviceIDJoypadGuide,
+	11: libretro.DeviceIDJoypadB,
+	12: libretro.DeviceIDJoypadA,
+	13: libretro.DeviceIDJoypadY,
+	14: libretro.DeviceIDJoypadX,
+}
+
+var retroPad [libretro.DeviceIDJoypadR3 + 1]bool
 
 func joystickCallback(joy int, event int) {
 	switch event {
@@ -40,8 +58,24 @@ func inputInit() {
 }
 
 func inputPoll() {
+	for i := range retroPad {
+		retroPad[i] = false
+	}
+
+	buttonState := glfw.GetJoystickButtons(glfw.Joystick1)
+
+	if len(buttonState) > 0 {
+		for k, v := range buttonBinds {
+			if glfw.Action(buttonState[k]) == glfw.Press {
+				retroPad[v] = true
+			}
+		}
+	}
+
 	for k, v := range keyBinds {
-		joy[v] = (window.GetKey(k) == glfw.Press)
+		if window.GetKey(k) == glfw.Press {
+			retroPad[v] = true
+		}
 	}
 
 	// Close the window when the user hits the Escape key.
@@ -55,7 +89,7 @@ func inputState(port uint, device uint32, index uint, id uint) int16 {
 		return 0
 	}
 
-	if id < 255 && joy[id] {
+	if id < 255 && retroPad[id] {
 		return 1
 	}
 	return 0
