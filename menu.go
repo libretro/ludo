635a9f9ee7e9ff26ae178fad2eed80a90f829850
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-05-01 21:26:23 +0700

    Make quick menu work

--- menu.go
+++ menu.go
@@ -30,21 +30,24 @@ func buildQuickMenu() entry {
 	menu.children = append(menu.children, entry{
 		label: "Save State",
 		callback: func() {
-			fmt.Println("Not implemented")
+			fmt.Println("[Menu]: Not implemented")
+			notify("Not implemented", 120)
 		},
 	})
 
 	menu.children = append(menu.children, entry{
 		label: "Load State",
 		callback: func() {
-			fmt.Println("Not implemented")
+			fmt.Println("[Menu]: Not implemented")
+			notify("Not implemented", 120)
 		},
 	})
 
 	menu.children = append(menu.children, entry{
 		label: "Take Screenshot",
 		callback: func() {
-			fmt.Println("Not implemented")
+			fmt.Println("[Menu]: Not implemented")
+			notify("Not implemented", 120)
 		},
 	})
 
@@ -65,20 +68,29 @@ func menuInput() {
 			currentMenu.ptr = 0
 		}
 	}
+
 	if pressed[0][libretro.DeviceIDJoypadUp] {
 		currentMenu.ptr--
 		if currentMenu.ptr < 0 {
 			currentMenu.ptr = len(currentMenu.children) - 1
 		}
 	}
+
+	if pressed[0][libretro.DeviceIDJoypadA] {
+		if currentMenu.children[currentMenu.ptr].callback != nil {
+			currentMenu.children[currentMenu.ptr].callback()
+		}
+	}
 }
 
 func renderMenuList() {
+	video.font.SetColor(0, 0, 0, 1.0)
+	video.font.Printf(60+2, 20+60+2, 0.5, currentMenu.label)
 	video.font.SetColor(1, 1, 1, 1.0)
-	video.font.Printf(60+1, 20+60, 0.5, currentMenu.label)
+	video.font.Printf(60, 20+60, 0.5, currentMenu.label)
 	for i, e := range currentMenu.children {
 		video.font.SetColor(0, 0, 0, 1.0)
-		video.font.Printf(100+1, 20+float32(70*(i+2))+1, 0.5, e.label)
+		video.font.Printf(100+2, 20+float32(70*(i+2))+2, 0.5, e.label)
 		if i == currentMenu.ptr {
 			video.font.SetColor(0.0, 1.0, 0.0, 1.0)
 		} else {
