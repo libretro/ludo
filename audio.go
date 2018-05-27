b1a091e65863a189cf5ab8db4ddb627a7c287cb1
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-05-28 00:05:20 +0700

    Clean audio

--- audio.go
+++ audio.go
@@ -1,24 +1,9 @@
 package main
 
-const bufSize = 1024 * 4
-
-var audio struct {
-	rate       int32
-	numBuffers int32
-	tmpBuf     [bufSize]byte
-	tmpBufPtr  int32
-	bufPtr     int32
-	resPtr     int32
-}
-
 func audioSetVolume(vol float32) {
 }
 
 func audioInit(rate int32) {
-}
-
-func audioWrite(buf []byte, size int32) int32 {
-	return 0
 }
 
 func audioSample(left int16, right int16) {
