8340325c6b560fd371ce1507a5a930cd9730bf1d
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-05-28 00:24:38 +0700

    Add openal back

--- audio.go
+++ audio.go
@@ -1,9 +1,12 @@
 package main
 
+import "golang.org/x/mobile/exp/audio/al"
+
 func audioSetVolume(vol float32) {
 }
 
 func audioInit(rate int32) {
+	al.OpenDevice()
 }
 
 func audioSample(left int16, right int16) {
