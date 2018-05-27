2f08cd4581c6f0c3afe63dcb56e6607442520e69
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-05-27 17:52:22 +0700

    Empty audio_test

--- audio_test.go
+++ audio_test.go
@@ -3,7 +3,6 @@ package main
 import "testing"
 
 func Test_fillInternalBuf(t *testing.T) {
-	audioInit(48000)
 	type args struct {
 		buf  []byte
 		size int32
@@ -12,24 +11,7 @@ func Test_fillInternalBuf(t *testing.T) {
 		name string
 		args args
 		want int32
-	}{
-		{
-			name: "Fill the buffer partially",
-			args: args{
-				buf:  make([]byte, 4096),
-				size: 3000,
-			},
-			want: 3000,
-		},
-		{
-			name: "Fill the buffer fully",
-			args: args{
-				buf:  make([]byte, 4096),
-				size: 3000,
-			},
-			want: 1096,
-		},
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := fillInternalBuf(tt.args.buf, tt.args.size); got != tt.want {
