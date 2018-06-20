df74bd3827ce0f78e6cbdf2f1f199c630d82bce7
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-06-21 00:11:25 +0700

    Apply icon scale to widget

--- menu_scene_settings.go
+++ menu_scene_settings.go
@@ -56,14 +56,14 @@ var widgets = map[string]func(*entry){
 	// Range widget for audio volume and similat float settings
 	"range": func(e *entry) {
 		fbw, fbh := window.GetFramebufferSize()
-		x := float32(fbw) - 650*menu.ratio - 128*menu.ratio
+		x := float32(fbw) - 650*menu.ratio - 256*e.scale*menu.ratio
 		y := float32(fbh)*e.yp - 3*menu.ratio
-		w := 128 * menu.ratio
+		w := 256 * e.scale * menu.ratio
 		h := 6 * menu.ratio
 		x1, y1, x2, y2, x3, y3, x4, y4 := xywhTo4points(x, y, w, h, float32(fbh))
 		drawQuad(x1, y1, x2, y2, x3, y3, x4, y4, color{1, 1, 1, e.iconAlpha / 4})
 
-		w = 128 * menu.ratio * e.value().(float32)
+		w = 256 * e.scale * menu.ratio * e.value().(float32)
 		x1, y1, x2, y2, x3, y3, x4, y4 = xywhTo4points(x, y, w, h, float32(fbh))
 		drawQuad(x1, y1, x2, y2, x3, y3, x4, y4, color{1, 1, 1, e.iconAlpha})
 	},
