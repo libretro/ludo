64cfa2e8eab9e2dc53f63d7c03854e9cc5554346
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-06-20 21:47:30 +0700

    Test fastForwardTweens

--- menu_test.go
+++ menu_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"reflect"
 	"testing"
+
+	"github.com/tanema/gween"
+	"github.com/tanema/gween/ease"
 )
 
 type CtxMock struct{}
@@ -46,6 +49,47 @@ func Test_menuInit(t *testing.T) {
 	t.Run("Warps at the quick menu if a game is launched", func(t *testing.T) {
 		got := menu.stack[len(menu.stack)-1].Entry().label
 		want := "Quick Menu"
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("No tweens are left after warping to quick menu", func(t *testing.T) {
+		got := len(menu.tweens)
+		want := 0
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got = %v, want %v", got, want)
+		}
+	})
+}
+
+func Test_fastForwardTweens(t *testing.T) {
+	foo := float32(5)
+	menu.tweens[&foo] = gween.New(foo, 25, 0.015, ease.OutSine)
+	bar := float32(-10)
+	menu.tweens[&bar] = gween.New(bar, 0, 0.9, ease.OutSine)
+
+	fastForwardTweens()
+
+	t.Run("No tweens are left", func(t *testing.T) {
+		got := len(menu.tweens)
+		want := 0
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Tweened vars have the right value", func(t *testing.T) {
+		got := foo
+		want := float32(25)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got = %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Tweened vars have the right value", func(t *testing.T) {
+		got := bar
+		want := float32(0)
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got = %v, want %v", got, want)
 		}
