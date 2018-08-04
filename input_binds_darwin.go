6081dd723c086f822b780b243c1b4c0d1bc2a5b5
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-08-04 12:41:40 +0700

    Bindings for 8BITDO NES30 PRO GamePad (Wired) on OSX

--- input_binds_darwin.go
+++ input_binds_darwin.go
@@ -5,6 +5,7 @@ import "github.com/kivutar/go-playthemall/libretro"
 var joyBinds = map[string]joybinds{
 	"Xbox 360 Wired Controller": xbox360JoyBinds,
 	"Wireless Controller":       ds4JoyBinds,
+	"8Bitdo NES30 Pro":          nes30proJoyBinds,
 }
 
 // Joypad bindings fox Xbox360 pad on OSX
@@ -53,4 +54,24 @@ var ds4JoyBinds = joybinds{
 	bind{btn, 15, 0, 0}: libretro.DeviceIDJoypadRight,
 	bind{btn, 16, 0, 0}: libretro.DeviceIDJoypadDown,
 	bind{btn, 17, 0, 0}: libretro.DeviceIDJoypadLeft,
+}
+
+// Joypad bindings fox the 8BITDO NES30 PRO GamePad (Wired) on OSX
+var nes30proJoyBinds = joybinds{
+	bind{btn, 0, 0, 0}:  libretro.DeviceIDJoypadA,
+	bind{btn, 1, 0, 0}:  libretro.DeviceIDJoypadB,
+	bind{btn, 3, 0, 0}:  libretro.DeviceIDJoypadX,
+	bind{btn, 4, 0, 0}:  libretro.DeviceIDJoypadY,
+	bind{btn, 6, 0, 0}:  libretro.DeviceIDJoypadL,
+	bind{btn, 7, 0, 0}:  libretro.DeviceIDJoypadR,
+	bind{btn, 8, 0, 0}:  libretro.DeviceIDJoypadL2,
+	bind{btn, 9, 0, 0}:  libretro.DeviceIDJoypadR2,
+	bind{btn, 10, 0, 0}: libretro.DeviceIDJoypadSelect,
+	bind{btn, 11, 0, 0}: libretro.DeviceIDJoypadStart,
+	bind{btn, 13, 0, 0}: libretro.DeviceIDJoypadL3,
+	bind{btn, 14, 0, 0}: libretro.DeviceIDJoypadR3,
+	bind{btn, 15, 0, 0}: libretro.DeviceIDJoypadUp,
+	bind{btn, 16, 0, 0}: libretro.DeviceIDJoypadRight,
+	bind{btn, 17, 0, 0}: libretro.DeviceIDJoypadDown,
+	bind{btn, 18, 0, 0}: libretro.DeviceIDJoypadLeft,
 }
