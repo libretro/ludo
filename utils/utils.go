1f216da7ec05a13bd01a9dfbe44ee95afc7fdac2
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-08-19 17:22:33 +0700

    Code cleaning and doc

--- utils/utils.go
+++ utils/utils.go
@@ -1,33 +1,25 @@
 package utils
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 )
 
+// Slurp reads a file completely and returns the content as a []byte.
 func Slurp(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-	stats, err := f.Stat()
-	if err != nil {
-		return nil, err
-	}
-	bytes := make([]byte, stats.Size())
-	buffer := bufio.NewReader(f)
-	_, err = buffer.Read(bytes)
-	if err != nil {
-		return nil, err
-	}
-	return bytes, nil
+	return ioutil.ReadAll(f)
 }
 
+// StringInSlice check wether a string is contain in a string slice.
 func StringInSlice(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
@@ -37,6 +29,7 @@ func StringInSlice(a string, list []string) bool {
 	return false
 }
 
+// Filename returns the name of a file, without the path and extension.
 func Filename(path string) string {
 	name := filepath.Base(path)
 	ext := filepath.Ext(name)
@@ -51,6 +44,8 @@ func (writer logWriter) Write(bytes []byte) (int, error) {
 	return fmt.Print(string(bytes))
 }
 
+// CaptureOutput executes a function and capture all the text outputed to logs.
+// Used in unit tests.
 func CaptureOutput(f func()) string {
 	var buf bytes.Buffer
 	log.SetFlags(0)
