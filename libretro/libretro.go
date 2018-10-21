4cfca892c3b425ff9704a3d9a8c3a9a29a14c49f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-10-21 16:04:56 +0700

    Start building on travis windows (#65)
    * Start building on travis windows
    
    * Add openal
    
    * Try to fix go lint
    
    * Don't run go vet on windows
    
    * Install make
    
    * Code cleaning
    
    * Disable appveyor

--- libretro/libretro.go
+++ libretro/libretro.go
@@ -4,8 +4,7 @@ Package libretro is a cgo binding for the libretro API.
 Libretro is a simple but powerful development interface that allows for the easy creation of
 emulators, games and multimedia applications that can plug straight into any libretro-compatible
 frontend. This development interface is open to others so that they can run these pluggable emulator
-and game cores also in their own programs or devices.
-*/
+and game cores also in their own programs or devices. */
 package libretro
 
 /*
