875292157965bd2b2a2c56599674a5e2e6fb8154
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-01-19 13:14:58 +0700

    DS3 bindings for Linux

--- input/binds_linux.go
+++ input/binds_linux.go
@@ -7,6 +7,7 @@ var joyBinds = map[string]joybinds{
 	"Microsoft X-Box One S pad":                          xboxOneJoyBinds,
 	"Sony Computer Entertainment Wireless Controller":    ds4JoyBinds,
 	"Sony Interactive Entertainment Wireless Controller": ds4JoyBinds,
+	"Sony PLAYSTATION(R)3 Controller":                    ds3JoyBinds,
 }
 
 // Joypad bindings fox Xbox360 pad on Linux
@@ -70,4 +71,25 @@ var ds4JoyBinds = joybinds{
 	bind{axis, 6, 1, 0.5}:   libretro.DeviceIDJoypadRight,
 	bind{axis, 7, -1, -0.5}: libretro.DeviceIDJoypadUp,
 	bind{axis, 7, 1, 0.5}:   libretro.DeviceIDJoypadDown,
+}
+
+// Joypad bindings fox DualShock 3 pad on Linux
+var ds3JoyBinds = joybinds{
+	bind{btn, 0, 0, 0}:  libretro.DeviceIDJoypadB,
+	bind{btn, 1, 0, 0}:  libretro.DeviceIDJoypadA,
+	bind{btn, 2, 0, 0}:  libretro.DeviceIDJoypadX,
+	bind{btn, 3, 0, 0}:  libretro.DeviceIDJoypadY,
+	bind{btn, 4, 0, 0}:  libretro.DeviceIDJoypadL,
+	bind{btn, 5, 0, 0}:  libretro.DeviceIDJoypadR,
+	bind{btn, 6, 0, 0}:  libretro.DeviceIDJoypadL2,
+	bind{btn, 7, 0, 0}:  libretro.DeviceIDJoypadR2,
+	bind{btn, 8, 0, 0}:  libretro.DeviceIDJoypadSelect,
+	bind{btn, 9, 0, 0}:  libretro.DeviceIDJoypadStart,
+	bind{btn, 10, 0, 0}: ActionMenuToggle,
+	bind{btn, 11, 0, 0}: libretro.DeviceIDJoypadL3,
+	bind{btn, 12, 0, 0}: libretro.DeviceIDJoypadR3,
+	bind{btn, 13, 0, 0}: libretro.DeviceIDJoypadUp,
+	bind{btn, 14, 0, 0}: libretro.DeviceIDJoypadDown,
+	bind{btn, 15, 0, 0}: libretro.DeviceIDJoypadLeft,
+	bind{btn, 16, 0, 0}: libretro.DeviceIDJoypadRight,
 }
