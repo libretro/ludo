bdc2e8e9e4b34bf5dadc7b527ebef0ac8777dc2f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-01-19 13:38:55 +0700

    8BITDO NES30 PRO Linux bindings

--- input/binds_linux.go
+++ input/binds_linux.go
@@ -9,6 +9,7 @@ var joyBinds = map[string]joybinds{
 	"Sony Interactive Entertainment Wireless Controller": ds4JoyBinds,
 	"Sony PLAYSTATION(R)3 Controller":                    ds3JoyBinds,
 	"SHANWAN PS3 GamePad":                                ds3JoyBinds,
+	"8Bitdo NES30 Pro   8Bitdo NES30 Pro":                nes30proJoyBinds,
 }
 
 // Joypad bindings fox Xbox360 pad on Linux
@@ -93,4 +94,24 @@ var ds3JoyBinds = joybinds{
 	bind{btn, 14, 0, 0}: libretro.DeviceIDJoypadDown,
 	bind{btn, 15, 0, 0}: libretro.DeviceIDJoypadLeft,
 	bind{btn, 16, 0, 0}: libretro.DeviceIDJoypadRight,
+}
+
+// Joypad bindings fox the 8BITDO NES30 PRO GamePad (Wired) on Linux
+var nes30proJoyBinds = joybinds{
+	bind{btn, 0, 0, 0}:      libretro.DeviceIDJoypadA,
+	bind{btn, 1, 0, 0}:      libretro.DeviceIDJoypadB,
+	bind{btn, 3, 0, 0}:      libretro.DeviceIDJoypadX,
+	bind{btn, 4, 0, 0}:      libretro.DeviceIDJoypadY,
+	bind{btn, 6, 0, 0}:      libretro.DeviceIDJoypadL,
+	bind{btn, 7, 0, 0}:      libretro.DeviceIDJoypadR,
+	bind{btn, 8, 0, 0}:      libretro.DeviceIDJoypadL2,
+	bind{btn, 9, 0, 0}:      libretro.DeviceIDJoypadR2,
+	bind{btn, 10, 0, 0}:     libretro.DeviceIDJoypadSelect,
+	bind{btn, 11, 0, 0}:     libretro.DeviceIDJoypadStart,
+	bind{btn, 13, 0, 0}:     libretro.DeviceIDJoypadL3,
+	bind{btn, 14, 0, 0}:     libretro.DeviceIDJoypadR3,
+	bind{axis, 6, -1, -0.5}: libretro.DeviceIDJoypadLeft,
+	bind{axis, 6, 1, 0.5}:   libretro.DeviceIDJoypadRight,
+	bind{axis, 7, -1, -0.5}: libretro.DeviceIDJoypadUp,
+	bind{axis, 7, 1, 0.5}:   libretro.DeviceIDJoypadDown,
 }
