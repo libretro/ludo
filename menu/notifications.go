bc27681bb9285852f1e36807b28d7a02233ddd13
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-01-21 16:25:31 +0700

    Improve notifications fading effect

--- menu/notifications.go
+++ menu/notifications.go
@@ -9,15 +9,30 @@ import (
 func RenderNotifications() {
 	fbw, fbh := vid.Window.GetFramebufferSize()
 	vid.Font.UpdateResolution(fbw, fbh)
-	for i, n := range notifications.List() {
-		a := float32(n.Frames) / 120
+	var h float32 = 75
+	stack := h
+	for _, n := range notifications.List() {
+		fading := float32(n.Frames) / 120
+		if fading > 1 {
+			fading = 1
+		}
+		offset := fading*20 - 20
 		lw := vid.Font.Width(0.5*menu.ratio, n.Message)
 		vid.DrawRoundedRect(
-			25*menu.ratio, (float32(75+75*i)-46)*menu.ratio,
-			lw+40*menu.ratio, 70*menu.ratio, 0.25,
-			video.Color{R: 0.4, G: 0.4, B: 0, A: a})
-		vid.Font.SetColor(1, 1, 0.85, a)
+			25*menu.ratio,
+			(stack+offset-46)*menu.ratio,
+			lw+40*menu.ratio,
+			70*menu.ratio,
+			0.25,
+			video.Color{R: 0.4, G: 0.4, B: 0, A: fading},
+		)
+		vid.Font.SetColor(1, 1, 0.85, fading)
 		vid.Font.Printf(
-			45*menu.ratio, float32(75+75*i)*menu.ratio, 0.5*menu.ratio, n.Message)
+			45*menu.ratio,
+			(stack+offset)*menu.ratio,
+			0.5*menu.ratio,
+			n.Message,
+		)
+		stack += h + offset
 	}
 }
