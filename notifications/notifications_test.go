f8420cf0b1f8f3caf6938b1b1d396b59190dd770
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-01-24 16:59:19 +0700

    Fix tests

--- notifications/notifications_test.go
+++ notifications/notifications_test.go
@@ -11,14 +11,14 @@ import (
 func Test_List(t *testing.T) {
 	Clear()
 	t.Run("Returns the notifications", func(t *testing.T) {
-		Display("Test1", 240)
-		Display("Test2", 240)
-		Display("Test3", 240)
+		Display("error", "Test1", 240)
+		Display("error", "Test2", 240)
+		Display("error", "Test3", 240)
 		got := List()
 		want := []Notification{
-			Notification{Message: "Test1", Frames: 240},
-			Notification{Message: "Test2", Frames: 240},
-			Notification{Message: "Test3", Frames: 240},
+			Notification{Severity: "error", Message: "Test1", Frames: 240},
+			Notification{Severity: "error", Message: "Test2", Frames: 240},
+			Notification{Severity: "error", Message: "Test3", Frames: 240},
 		}
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got = %v, want %v", got, want)
@@ -29,14 +29,14 @@ func Test_List(t *testing.T) {
 func Test_Display(t *testing.T) {
 	Clear()
 	t.Run("Stacks notifications correctly", func(t *testing.T) {
-		Display("Test1", 240)
-		Display("Test2", 240)
-		Display("Test3", 240)
+		Display("error", "Test1", 240)
+		Display("info", "Test2", 240)
+		Display("warning", "Test3", 240)
 		got := notifications
 		want := []Notification{
-			Notification{Message: "Test1", Frames: 240},
-			Notification{Message: "Test2", Frames: 240},
-			Notification{Message: "Test3", Frames: 240},
+			Notification{Severity: "error", Message: "Test1", Frames: 240},
+			Notification{Severity: "info", Message: "Test2", Frames: 240},
+			Notification{Severity: "warning", Message: "Test3", Frames: 240},
 		}
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got = %v, want %v", got, want)
@@ -47,7 +47,7 @@ func Test_Display(t *testing.T) {
 func Test_DisplayAndLog(t *testing.T) {
 	Clear()
 	t.Run("Format message properly", func(t *testing.T) {
-		DisplayAndLog("Tests", "Joypad #%d loaded with name %s.", 3, "Foo")
+		DisplayAndLog("info", "Tests", "Joypad #%d loaded with name %s.", 3, "Foo")
 		got := notifications[0].Message
 		want := "Joypad #3 loaded with name Foo."
 		if got != want {
@@ -57,7 +57,7 @@ func Test_DisplayAndLog(t *testing.T) {
 
 	Clear()
 	t.Run("Format simple message properly", func(t *testing.T) {
-		DisplayAndLog("Tests", "Hello world.")
+		DisplayAndLog("info", "Tests", "Hello world.")
 		got := notifications[0].Message
 		want := "Hello world."
 		if got != want {
@@ -68,7 +68,7 @@ func Test_DisplayAndLog(t *testing.T) {
 	Clear()
 	t.Run("Logs to stdout if verbose", func(t *testing.T) {
 		state.Global.Verbose = true
-		got := utils.CaptureOutput(func() { DisplayAndLog("Test", "Joypad #%d loaded with name %s.", 3, "Foo") })
+		got := utils.CaptureOutput(func() { DisplayAndLog("info", "Test", "Joypad #%d loaded with name %s.", 3, "Foo") })
 		want := "[Test]: Joypad #3 loaded with name Foo.\n"
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got = %v, want %v", got, want)
@@ -78,7 +78,7 @@ func Test_DisplayAndLog(t *testing.T) {
 	Clear()
 	t.Run("Logs nothing if not verbose", func(t *testing.T) {
 		state.Global.Verbose = false
-		got := utils.CaptureOutput(func() { DisplayAndLog("Test", "Joypad #%d loaded with name %s.", 3, "Foo") })
+		got := utils.CaptureOutput(func() { DisplayAndLog("info", "Test", "Joypad #%d loaded with name %s.", 3, "Foo") })
 		want := ""
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got = %v, want %v", got, want)
@@ -89,11 +89,11 @@ func Test_DisplayAndLog(t *testing.T) {
 func Test_processNotifications(t *testing.T) {
 	Clear()
 	t.Run("Delete outdated notification", func(t *testing.T) {
-		Display("Test1", 5)
-		Display("Test1", 4)
-		Display("Test1", 3)
-		Display("Test2", 2)
-		Display("Test3", 1)
+		Display("error", "Test1", 5)
+		Display("error", "Test1", 4)
+		Display("error", "Test1", 3)
+		Display("error", "Test2", 2)
+		Display("error", "Test3", 1)
 		Process()
 		Process()
 		got := len(notifications)
@@ -107,9 +107,9 @@ func Test_processNotifications(t *testing.T) {
 func Test_Clear(t *testing.T) {
 	Clear()
 	t.Run("Empties the notification list", func(t *testing.T) {
-		Display("Test1", 240)
-		Display("Test2", 240)
-		Display("Test3", 240)
+		Display("error", "Test1", 240)
+		Display("error", "Test2", 240)
+		Display("error", "Test3", 240)
 		Clear()
 		got := len(notifications)
 		want := 0
