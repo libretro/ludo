a9c8589194b64c0a5a117ee3dae8fe1331ea26b6
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-01-29 16:41:31 +0700

    Remove AL completely

--- audio/audio.go
+++ audio/audio.go
@@ -2,22 +2,6 @@
 // callbacks Sample and SampleBatch for the libretro implementation.
 package audio
 
-import (
-	"log"
-
-	"golang.org/x/mobile/exp/audio/al"
-)
-
-const bufSize = 1024 * 4
-
-var audio struct {
-	rate       int32
-	numBuffers int32
-	tmpBuf     [bufSize]byte
-	tmpBufPtr  int32
-	resPtr     int32
-}
-
 // SetVolume sets the audio volume
 func SetVolume(vol float32) {
 }
@@ -25,19 +9,6 @@ func SetVolume(vol float32) {
 // Init initializes the audio package. It opens the AL devices, sets the number of buffers, the
 // volume and the source.
 func Init(rate int32) {
-	err := al.OpenDevice()
-	if err != nil {
-		log.Println(err)
-	}
-
-	audio.rate = rate
-	audio.numBuffers = 4
-
-	log.Printf("[OpenAL]: Using %v buffers of %v bytes.\n", audio.numBuffers, bufSize)
-
-	audio.resPtr = audio.numBuffers
-	audio.tmpBufPtr = 0
-	audio.tmpBuf = [bufSize]byte{}
 }
 
 func write(buf []byte, size int32) int32 {
