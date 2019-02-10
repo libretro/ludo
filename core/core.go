6b382775eb333ab59cac3b7b9e0ac892e22a7f99
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-02-10 14:58:14 +0700

    Fix input on pcsx_rearmed

--- core/core.go
+++ core/core.go
@@ -167,9 +167,8 @@ func LoadGame(gamePath string) error {
 	state.Global.CoreRunning = true
 	state.Global.GamePath = gamePath
 
-	for p := uint(0); p < input.MaxPlayers; p++ {
-		state.Global.Core.SetControllerPortDevice(p, libretro.DeviceJoypad)
-	}
+	state.Global.Core.SetControllerPortDevice(0, libretro.DeviceJoypad)
+	state.Global.Core.SetControllerPortDevice(1, libretro.DeviceJoypad)
 
 	log.Println("[Core]: Game loaded: " + gamePath)
 	savefiles.LoadSRAM()
