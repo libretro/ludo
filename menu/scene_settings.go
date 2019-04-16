c4ff43c0e4daafa4b900d9d63748b7ae0cd0cccf
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-04-16 22:32:14 +0700

    Move wifi menu to the top of settings

--- menu/scene_settings.go
+++ menu/scene_settings.go
@@ -33,6 +33,17 @@ func buildSettings() Scene {
 	var list sceneSettings
 	list.label = "Settings"
 
+	if state.Global.LudOS {
+		list.children = append(list.children, entry{
+			label: "Wi-Fi",
+			icon:  "subsetting",
+			callbackOK: func() {
+				list.segueNext()
+				menu.stack = append(menu.stack, buildWiFi())
+			},
+		})
+	}
+
 	fields := structs.Fields(&settings.Current)
 	for _, f := range fields {
 		f := f
@@ -79,17 +90,6 @@ func buildSettings() Scene {
 				widget: widgets[f.Tag("widget")],
 			})
 		}
-	}
-
-	if state.Global.LudOS {
-		list.children = append(list.children, entry{
-			label: "Wi-Fi",
-			icon:  "subsetting",
-			callbackOK: func() {
-				list.segueNext()
-				menu.stack = append(menu.stack, buildWiFi())
-			},
-		})
 	}
 
 	list.segueMount()
