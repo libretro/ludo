356d645b67c1643c65d8a2101add6ad340f848b0
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-06-10 22:01:31 +0700

    Improve layout

--- menu/scene_dialog.go
+++ menu/scene_dialog.go
@@ -45,24 +45,61 @@ func (s *sceneDialog) update(dt float32) {
 
 func (s *sceneDialog) render() {
 	w, h := vid.Window.GetFramebufferSize()
-	vid.DrawRect(0, 0, float32(w), float32(h), 0, video.Color{R: 0.5, G: 0, B: 0, A: 1})
-	vid.Font.SetColor(1, 1, 1, 1)
+	fw := float32(w)
+	fh := float32(h)
+	vid.DrawRect(0, 0, fw, fh, 0, video.Color{R: 0, G: 0, B: 0, A: 0.85})
+
+	var width float32 = 1000
+	var height float32 = 400
+
+	vid.DrawRect(
+		fw/2-width/2*menu.ratio,
+		fh/2-height/2*menu.ratio,
+		width*menu.ratio,
+		height*menu.ratio,
+		0.05,
+		video.Color{R: 1, G: 1, B: 1, A: 1},
+	)
+
+	vid.Font.SetColor(0.8, 0.4, 0.1, 1)
 	msg1 := "A game is currently running."
-	lw1 := vid.Font.Width(0.6*menu.ratio, msg1)
-	vid.Font.Printf(float32(w)/2-lw1/2, float32(h)/2-60*menu.ratio, 0.6*menu.ratio, msg1)
+	lw1 := vid.Font.Width(0.7*menu.ratio, msg1)
+	vid.Font.Printf(fw/2-lw1/2, fh/2-120*menu.ratio+20*menu.ratio, 0.7*menu.ratio, msg1)
+	vid.Font.SetColor(0, 0, 0, 1)
 	msg2 := "If you have not saved yet, your progress will be lost."
-	lw2 := vid.Font.Width(0.6*menu.ratio, msg2)
-	vid.Font.Printf(float32(w)/2-lw2/2, float32(h)/2, 0.6*menu.ratio, msg2)
+	lw2 := vid.Font.Width(0.5*menu.ratio, msg2)
+	vid.Font.Printf(fw/2-lw2/2, fh/2-30*menu.ratio+20*menu.ratio, 0.5*menu.ratio, msg2)
 	msg3 := "Do you want to exit Ludo anyway?"
-	lw3 := vid.Font.Width(0.6*menu.ratio, msg3)
-	vid.Font.Printf(float32(w)/2-lw3/2, float32(h)/2+60*menu.ratio, 0.6*menu.ratio, msg3)
+	lw3 := vid.Font.Width(0.5*menu.ratio, msg3)
+	vid.Font.Printf(fw/2-lw3/2, fh/2+30*menu.ratio+20*menu.ratio, 0.5*menu.ratio, msg3)
+
+	c := video.Color{R: 0.25, G: 0.25, B: 0.25, A: 1}
+	vid.Font.SetColor(0.25, 0.25, 0.25, 1.0)
+
+	var margin float32 = 15
+
+	vid.DrawImage(
+		menu.icons["key-z"],
+		fw/2-width/2*menu.ratio+margin*menu.ratio,
+		fh/2+height/2*menu.ratio-70*menu.ratio-margin*menu.ratio,
+		70*menu.ratio, 70*menu.ratio, 1.0, c)
+	vid.Font.Printf(
+		fw/2-width/2*menu.ratio+margin*menu.ratio+70*menu.ratio,
+		fh/2+height/2*menu.ratio-23*menu.ratio-margin*menu.ratio,
+		0.5*menu.ratio,
+		"NO")
+
+	vid.DrawImage(
+		menu.icons["key-x"],
+		fw/2+width/2*menu.ratio-150*menu.ratio-margin*menu.ratio,
+		fh/2+height/2*menu.ratio-70*menu.ratio-margin*menu.ratio,
+		70*menu.ratio, 70*menu.ratio, 1.0, c)
+	vid.Font.Printf(
+		fw/2+width/2*menu.ratio-150*menu.ratio-margin*menu.ratio+70*menu.ratio,
+		fh/2+height/2*menu.ratio-23*menu.ratio-margin*menu.ratio,
+		0.5*menu.ratio,
+		"YES")
 }
 
 func (s *sceneDialog) drawHintBar() {
-	w, h := vid.Window.GetFramebufferSize()
-	vid.DrawRect(0, float32(h)-70*menu.ratio, float32(w), 70*menu.ratio, 0, video.Color{R: 0.75, G: 0.75, B: 0.75, A: 1})
-
-	var stack float32
-	stackHint(&stack, "key-z", "NO", h)
-	stackHint(&stack, "key-x", "YES", h)
 }
