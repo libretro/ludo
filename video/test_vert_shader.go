bd99584fe9843561067958ed60b8871679ebf369
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-10-11 23:16:35 +0700

    Add sdf rounded rect shader

---
+++ video/test_vert_shader.go
@@ -0,0 +1,26 @@
+package video
+
+var testVertexShader = `
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#define COMPAT_FRAGCOLOR FragColor
+out vec4 COMPAT_FRAGCOLOR;
+#else
+#define COMPAT_VARYING varying
+#define COMPAT_ATTRIBUTE attribute
+#define COMPAT_TEXTURE texture2D
+#define COMPAT_FRAGCOLOR gl_FragColor
+#endif
+
+COMPAT_ATTRIBUTE vec2 vert;
+COMPAT_ATTRIBUTE vec2 vertTexCoord;
+
+COMPAT_VARYING vec2 fragTexCoord;
+
+void main() {
+  fragTexCoord = vertTexCoord;
+  gl_Position = vec4(vert, 0.0, 1.0);
+}
+` + "\x00"
