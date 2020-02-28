0f9cdeec8ffd9c54a333c2308b940d6b620ea642
Jean-André Santoni <jean.andre.santoni@gmail.com>
2020-02-28 17:08:19 +0700

    Clean

--- video/fbo_notdarwin.go
+++ video/fbo_notdarwin.go
@@ -18,33 +18,34 @@ func (video *Video) InitFramebuffer() {
 
 	log.Printf("[Video]: Initializing HW render (%v x %v).\n", width, height)
 
-	gl.GenFramebuffers(1, &video.fboID)
-	gl.BindFramebuffer(gl.FRAMEBUFFER, video.fboID)
-
-	//gl.GenTextures(1, &video.texID)
 	gl.BindTexture(gl.TEXTURE_2D, video.texID)
 	gl.TexStorage2D(gl.TEXTURE_2D, 1, gl.RGBA8, int32(width), int32(height))
 
-	gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, video.texID, 0)
-
 	hw := state.Global.Core.HWRenderCallback
 
-	if hw.Depth && hw.Stencil {
-		gl.GenRenderbuffers(1, &video.rboID)
-		gl.BindRenderbuffer(gl.RENDERBUFFER, video.rboID)
-		gl.RenderbufferStorage(gl.RENDERBUFFER, gl.DEPTH24_STENCIL8, int32(width), int32(height))
+	gl.GenFramebuffers(1, &video.fboID)
 
-		gl.FramebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, video.rboID)
-	} else if hw.Depth {
+	if hw.Depth {
 		gl.GenRenderbuffers(1, &video.rboID)
-		gl.BindRenderbuffer(gl.RENDERBUFFER, video.rboID)
-		gl.RenderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, int32(width), int32(height))
-
-		gl.FramebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, video.rboID)
 	}
 
-	if hw.Depth || hw.Stencil {
+	gl.BindFramebuffer(gl.FRAMEBUFFER, video.fboID)
+	gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, video.texID, 0)
+
+	if hw.Depth {
+		gl.BindRenderbuffer(gl.RENDERBUFFER, video.rboID)
+		format := gl.DEPTH_COMPONENT16
+		if hw.Stencil {
+			format = gl.DEPTH24_STENCIL8
+		}
+		gl.RenderbufferStorage(gl.RENDERBUFFER, uint32(format), int32(width), int32(height))
 		gl.BindRenderbuffer(gl.RENDERBUFFER, 0)
+
+		if hw.Stencil {
+			gl.FramebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_STENCIL_ATTACHMENT, gl.RENDERBUFFER, video.rboID)
+		} else {
+			gl.FramebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, video.rboID)
+		}
 	}
 
 	// Default origin is top left
