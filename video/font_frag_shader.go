d3fe1de29f1f2a79a69bbff2b5c3799d53bc01c4
Jean-André Santoni <jean.andre.santoni@gmail.com>
2020-02-29 11:19:57 +0700

    Code cleaning

--- video/font_frag_shader.go
+++ video/font_frag_shader.go
@@ -13,12 +13,13 @@ out vec4 FragColor;
 #define COMPAT_TEXTURE texture2D
 #define COMPAT_FRAGCOLOR gl_FragColor
 #endif
-COMPAT_VARYING vec2 fragTexCoord;
+
 uniform sampler2D tex;
 uniform vec4 textColor;
-void main()
-{
-    vec4 sampled = vec4(1.0, 1.0, 1.0, COMPAT_TEXTURE(tex, fragTexCoord).r);
-    COMPAT_FRAGCOLOR = min(textColor, vec4(1.0, 1.0, 1.0, 1.0)) * sampled;
+COMPAT_VARYING vec2 fragTexCoord;
+
+void main() {
+	vec4 sampled = vec4(1.0, 1.0, 1.0, COMPAT_TEXTURE(tex, fragTexCoord).r);
+	COMPAT_FRAGCOLOR = min(textColor, vec4(1.0, 1.0, 1.0, 1.0)) * sampled;
 }
 ` + "\x00"
