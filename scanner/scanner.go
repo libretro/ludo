7bea82690f0b2e6df16117cd38d1e72be238f6b2
Legomountain13 <110042204+Legomountain13@users.noreply.github.com>
2024-02-10 10:05:26 -0600

    Fixed Atari 2600 ROM Scanning

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -135,7 +135,7 @@ func Scan(dir string, roms []string, games chan (dat.Game), n *ntf.Notification)
 			// Look for a matching game entry in the database
 			state.DB.FindByROMName(f, filepath.Base(f), 0, games)
 			n.Update(ntf.Info, strconv.Itoa(i)+"/"+strconv.Itoa(len(roms))+" "+f)
-		case ".32x", "a52", ".a78", ".col", ".crt", ".d64", ".pce", ".fds", ".gb", ".gba", ".gbc", ".gen", ".gg", ".ipf", ".j64", ".jag", ".lnx", ".md", ".n64", ".nes", ".ngc", ".nds", ".rom", ".sfc", ".sg", ".smc", ".smd", ".sms", ".ws", ".wsc":
+		case ".32x", ".a26", "a52", ".a78", ".col", ".crt", ".d64", ".pce", ".fds", ".gb", ".gba", ".gbc", ".gen", ".gg", ".ipf", ".j64", ".jag", ".lnx", ".md", ".n64", ".nes", ".ngc", ".nds", ".rom", ".sfc", ".sg", ".smc", ".smd", ".sms", ".ws", ".wsc":
 			bytes, err := ioutil.ReadFile(f)
 			if err != nil {
 				n.Update(ntf.Error, err.Error())
